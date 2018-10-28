048f4414b8b61d84e598eec9fcd6009f438fca5e
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-10-28 11:32:02 +0100

    replaced flatten method because of problems in frontend

--- src/network.js
+++ src/network.js
@@ -1,6 +1,7 @@
 const Node = require('./node');
 const QuorumSet = require('./quorum-set');
 const QuorumService = require('./quorum-service');
+const _ = require('lodash');
 
 class Network {
     _nodes: Array<Node>;
@@ -65,7 +66,7 @@ class Network {
     }
 
     createLinks(){
-        this._links = this._nodes
+        this._links = _.flatten(this._nodes
             .filter(node => node.active && !this._failingNodes.includes(node))
             .map(node => {
             return QuorumSet.getAllValidators(node.quorumSet)
@@ -81,7 +82,7 @@ class Network {
                     && !this._failingNodes.includes(node)*/
                 };
             })
-        }).flatten()
+        }));
     }
 
     isClusterLink(source, target){
