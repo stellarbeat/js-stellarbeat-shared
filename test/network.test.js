cc6b7d7547de83b7891847842a175069ff7a3221
pieterjan84 <pieterjan84@users.noreply.github.com>
2018-11-13 21:30:27 +0100

    latest crawl date fix

--- test/network.test.js
+++ test/network.test.js
@@ -2,7 +2,7 @@ const Network = require('../lib/network');
 const Node = require('../lib/node');
 
 let node1 = new Node();
-console.log(node1.dateUpdated)
+
 let node2 = new Node();
 
 let network = new Network([node1, node2]);
