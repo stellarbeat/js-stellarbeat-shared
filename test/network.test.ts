c9f2ba7e4bb1fff53c910e8b978d36495ed7ac0a
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-04-19 14:05:47 +0200

    added tests for inner quorum sets

--- test/network.test.ts
+++ test/network.test.ts
@@ -1,16 +1,17 @@
-import {Node, Network, generateTomlString} from '../src';
+import {Node, Network, QuorumSet, generateTomlString} from '../src';
 
 jest.mock('./../src/quorum-set-toml-generator');
 
 let node1 = new Node('localhost', 20, 'a');
 let node2 = new Node('localhost', 20, 'b');
 let node3 = new Node('localhost', 20, 'c');
+let node4 = new Node('localhost', 20, 'c');
 
 node2.quorumSet.validators.push(node1.publicKey);
 node3.quorumSet.validators.push(node1.publicKey);
+node4.quorumSet.innerQuorumSets.push(new QuorumSet('hashkey', 1, ['a']));
 
-let network = new Network([node1, node2, node3]);
-console.log(node1.quorumSet.validators);
+let network = new Network([node1, node2, node3, node4]);
 
 test('getLatestCrawlDate', () => {
     expect(network.latestCrawlDate).toEqual(node2.dateUpdated);
@@ -22,5 +23,5 @@ test('getQuorumSetTomlConfig', () => {
 });
 
 test('getTrustingNodes', () => {
-    expect(network.getTrustingNodes(node1)).toEqual([node2, node3]);
+    expect(network.getTrustingNodes(node1)).toEqual([node2, node3, node4]);
 });
