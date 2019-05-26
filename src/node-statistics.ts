8ac25daa0e3e53a415775e50bee9c612ce98354f
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-05-26 13:15:49 +0200

    added 24h and 7 day stats

--- src/node-statistics.ts
+++ src/node-statistics.ts
@@ -1,11 +1,17 @@
 export class NodeStatistics {
-    //todo: store measurement time series for 24h results, monthly results,...?
-    _activeCounter: number;
-    _overLoadedCounter: number;
-    private _validatingCounter: number;
-    _activeInLastCrawl: boolean;
-    _overLoadedInLastCrawl: boolean;
-    private _validatingInLastCrawl: boolean = false;
+    protected _activeCounter: number;
+    protected _overLoadedCounter: number;
+    protected _validatingCounter: number;
+    protected _activeInLastCrawl: boolean;
+    protected _overLoadedInLastCrawl: boolean;
+    protected _validatingInLastCrawl: boolean = false;
+    protected _active7DaysPercentage: number = 0;
+    protected _overLoaded7DaysPercentage: number = 0;
+    protected _validating7DaysPercentage: number = 0;
+    protected _active24HoursPercentage: number = 0;
+    protected _overLoaded24HoursPercentage: number = 0;
+    protected _validating24HoursPercentage: number = 0;
+
 
     constructor(activeCounter: number = 0, overLoadedCounter:number = 0, validatingCounter: number = 0) {
         this._activeCounter = activeCounter;
@@ -124,6 +130,54 @@ export class NodeStatistics {
         }
     }
 
+    get active7DaysPercentage(): number {
+        return this._active7DaysPercentage;
+    }
+
+    set active7DaysPercentage(value: number) {
+        this._active7DaysPercentage = value;
+    }
+
+    get overLoaded7DaysPercentage(): number {
+        return this._overLoaded7DaysPercentage;
+    }
+
+    set overLoaded7DaysPercentage(value: number) {
+        this._overLoaded7DaysPercentage = value;
+    }
+
+    get validating7DaysPercentage(): number {
+        return this._validating7DaysPercentage;
+    }
+
+    set validating7DaysPercentage(value: number) {
+        this._validating7DaysPercentage = value;
+    }
+
+    get active24HoursPercentage(): number {
+        return this._active24HoursPercentage;
+    }
+
+    set active24HoursPercentage(value: number) {
+        this._active24HoursPercentage = value;
+    }
+
+    get overLoaded24HoursPercentage(): number {
+        return this._overLoaded24HoursPercentage;
+    }
+
+    set overLoaded24HoursPercentage(value: number) {
+        this._overLoaded24HoursPercentage = value;
+    }
+
+    get validating24HoursPercentage(): number {
+        return this._validating24HoursPercentage;
+    }
+
+    set validating24HoursPercentage(value: number) {
+        this._validating24HoursPercentage = value;
+    }
+
     toJSON():Object {
         return {
             activeCounter: this.activeCounter,
@@ -133,7 +187,13 @@ export class NodeStatistics {
             overLoadedInLastCrawl: this.overLoadedInLastCrawl,
             validatingInLastCrawl: this.validatingInLastCrawl,
             validatingCounter: this.validatingCounter,
-            validatingRating: this.validatingRating
+            validatingRating: this.validatingRating,
+            active7DaysPercentage: this.active7DaysPercentage,
+            overLoaded7DaysPercentage: this.overLoaded7DaysPercentage,
+            validating7DaysPercentage: this.validating7DaysPercentage,
+            active24HoursPercentage: this.active24HoursPercentage,
+            overLoaded24HoursPercentage: this.overLoaded24HoursPercentage,
+            validating24HoursPercentage: this.validating24HoursPercentage
         };
     };
 
@@ -161,6 +221,19 @@ export class NodeStatistics {
 
         if(nodeStatisticsObject.validatingCounter !== undefined && nodeStatisticsObject.validatingCounter !== null)
             newNodeStatistics.validatingCounter = nodeStatisticsObject.validatingCounter;
+
+        if(nodeStatisticsObject.active7DaysPercentage)
+            newNodeStatistics.active7DaysPercentage = nodeStatisticsObject.active7DaysPercentage;
+        if(nodeStatisticsObject.overLoaded7DaysPercentage)
+            newNodeStatistics.overLoaded7DaysPercentage = nodeStatisticsObject.overLoaded7DaysPercentage;
+        if(nodeStatisticsObject.validating7DaysPercentage)
+            newNodeStatistics.validating7DaysPercentage = nodeStatisticsObject.validating7DaysPercentage;
+        if(nodeStatisticsObject.active24HoursPercentage)
+            newNodeStatistics.active24HoursPercentage = nodeStatisticsObject.active24HoursPercentage;
+        if(nodeStatisticsObject.overLoaded24HoursPercentage)
+            newNodeStatistics.overLoaded24HoursPercentage = nodeStatisticsObject.overLoaded24HoursPercentage;
+        if(nodeStatisticsObject.validating24HoursPercentage)
+            newNodeStatistics.validating24HoursPercentage = nodeStatisticsObject.validating24HoursPercentage;
 
         return newNodeStatistics;
     }
