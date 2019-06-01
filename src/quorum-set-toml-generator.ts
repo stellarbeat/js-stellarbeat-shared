ff9ddb8dea37fdb62dfbfb71b3af99e6840c998d
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-06-01 07:51:45 +0200

    fixed toml threshold bug

--- src/quorum-set-toml-generator.ts
+++ src/quorum-set-toml-generator.ts
@@ -1,10 +1,9 @@
 import {Node, QuorumSet} from "./index";
 
 //todo: use toml nodejs package
-function getThresholdPercentage(quorumSet:QuorumSet):number {
-    return Math.round(
-        (quorumSet.threshold / (quorumSet.validators.length + quorumSet.innerQuorumSets.length)) * 100
-    );
+function getThresholdPercentage(quorumSet: QuorumSet): number {
+    return Math.ceil((((quorumSet.threshold - 1) * 100) + 1)
+        / (quorumSet.validators.length + quorumSet.innerQuorumSets.length));
 }
 
 function getValidatorsStringPart(quorumSet: QuorumSet, publicKeysToNodesMap: Map<string, Node>) {
@@ -21,13 +20,13 @@ function getValidatorsStringPart(quorumSet: QuorumSet, publicKeysToNodesMap: Map
     return validatorsStringPart;
 }
 
-export function generateTomlString(quorumSet:QuorumSet, publicKeysToNodesMap:Map<string, Node>, prefix:string = ""):string {
+export function generateTomlString(quorumSet: QuorumSet, publicKeysToNodesMap: Map<string, Node>, prefix: string = ""): string {
     let tomlString = '[QUORUM_SET' + prefix + ']\n';
     tomlString += 'THRESHOLD_PERCENT=' + getThresholdPercentage(quorumSet) + '\n';
     tomlString += getValidatorsStringPart(quorumSet, publicKeysToNodesMap);
     tomlString += ']\n';
 
-    for(let i = 0; i<quorumSet.innerQuorumSets.length; i++) {
+    for (let i = 0; i < quorumSet.innerQuorumSets.length; i++) {
         let newPrefix = prefix + '.' + (i + 1);
         tomlString += generateTomlString(quorumSet.innerQuorumSets[i], publicKeysToNodesMap, newPrefix);
     }
