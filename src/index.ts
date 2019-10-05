abe79a991fd729874b6290eca654ba4bb06d5212
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-10-05 11:09:50 +0200

    exported timeBuckets type

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export {default as QuorumService} from './quorum-service';
 export {QuorumSlicesGenerator} from './quorum-slices-generator'
 export {QuorumSet} from './quorum-set';
 export {QuorumSetService} from './quorum-set-service';
-export {NodeStatistics} from './node-statistics';
+export {NodeStatistics, TimeBucketAverages} from './node-statistics';
 export {NodeGeoData} from './node-geo-data';
 export {getPublicKeysToNodesMap} from './public-keys-to-nodes-mapper';
 export {generateTomlString} from './quorum-set-toml-generator';
