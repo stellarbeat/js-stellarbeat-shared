6340cb4204669a59aadff789bb94b11378bf0670
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-01-12 10:30:31 +0100

    cleaned up organization entity

--- src/organization.ts
+++ src/organization.ts
@@ -3,148 +3,29 @@ import {OrganizationId} from "./network";
 type PublicKey = string;
 
 export class Organization {
-    private _id:OrganizationId;
-    private _name:string;
-    private _dba?: string;
-    private _url?: string;
-    private _logo?: string;
-    private _description?: string;
-    private _physicalAddress?: string;
-    private _physicalAddressAttestation?: string;
-    private _phoneNumber?: string;
-    private _phoneNumberAttestation?:string;
-    private _keybase?: string;
-    private _twitter?: string;
-    private _github?: string;
-    private _officialEmail?: string;
-    private _validators: PublicKey[] = [];
-    private _subQuorum24HoursAvailability: number = 0;
-    private _subQuorum30DaysAvailability: number = 0;
+    public readonly id:OrganizationId;
+    public readonly name:string;
+    public dba?: string;
+    public url?: string;
+    public logo?: string;
+    public description?: string;
+    public physicalAddress?: string;
+    public physicalAddressAttestation?: string;
+    public phoneNumber?: string;
+    public phoneNumberAttestation?:string;
+    public keybase?: string;
+    public twitter?: string;
+    public github?: string;
+    public officialEmail?: string;
+    public validators: PublicKey[] = [];
+    public subQuorum24HoursAvailability: number = 0;
+    public subQuorum30DaysAvailability: number = 0;
 
     public dateDiscovered?: Date;
 
     constructor(id:string, name:string) {
-        this._id = id;
-        this._name = name;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(value) {
-        this._name = value;
-    }
-
-    get dba() {
-        return this._dba;
-    }
-
-    set dba(value) {
-        this._dba = value;
-    }
-
-    get url() {
-        return this._url;
-    }
-
-    set url(value) {
-        this._url = value;
-    }
-
-    get logo() {
-        return this._logo;
-    }
-
-    set logo(value) {
-        this._logo = value;
-    }
-
-    get description() {
-        return this._description;
-    }
-
-    set description(value) {
-        this._description = value;
-    }
-
-    get physicalAddress() {
-        return this._physicalAddress;
-    }
-
-    set physicalAddress(value) {
-        this._physicalAddress = value;
-    }
-
-    get physicalAddressAttestation() {
-        return this._physicalAddressAttestation;
-    }
-
-    set physicalAddressAttestation(value) {
-        this._physicalAddressAttestation = value;
-    }
-
-    get phoneNumber() {
-        return this._phoneNumber;
-    }
-
-    set phoneNumber(value) {
-        this._phoneNumber = value;
-    }
-
-    get phoneNumberAttestation() {
-        return this._phoneNumberAttestation;
-    }
-
-    set phoneNumberAttestation(value) {
-        this._phoneNumberAttestation = value;
-    }
-
-    get keybase() {
-        return this._keybase;
-    }
-
-    set keybase(value) {
-        this._keybase = value;
-    }
-
-    get twitter() {
-        return this._twitter;
-    }
-
-    set twitter(value) {
-        this._twitter = value;
-    }
-
-    get github() {
-        return this._github;
-    }
-
-    set github(value) {
-        this._github = value;
-    }
-
-    get officialEmail() {
-        return this._officialEmail;
-    }
-
-    set officialEmail(value) {
-        this._officialEmail = value;
-    }
-
-    get validators(): PublicKey[] {
-        return this._validators;
-    }
-
-    set validators(value: PublicKey[]) {
-        this._validators = value;
-    }
-
-    get id() {
-        return this._id;
-    }
-    set id(value) {
-        this._id = value;
+        this.id = id;
+        this.name = name;
     }
 
     get subQuorumFailAt(): number {
@@ -155,25 +36,9 @@ export class Organization {
         return Math.floor(this.validators.length - (this.validators.length - 1) / 2); //simple majority
     }
 
-    get subQuorum24HoursAvailability(): number {
-        return this._subQuorum24HoursAvailability;
-    }
-
-    set subQuorum24HoursAvailability(value: number) {
-        this._subQuorum24HoursAvailability = value;
-    }
-
-    get subQuorum30DaysAvailability(): number {
-        return this._subQuorum30DaysAvailability;
-    }
-
-    set subQuorum30DaysAvailability(value: number) {
-        this._subQuorum30DaysAvailability = value;
-    }
-
     public toJSON() :Object {
         return {
-            id: this._id,
+            id: this.id,
             name: this.name,
             dba: this.dba,
             url: this.url,
