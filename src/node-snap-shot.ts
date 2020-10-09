1f4c7d388d2b7fcba9ea8dfa949a44201d9a4950
pieterjan84 <pieterjan84@users.noreply.github.com>
2020-10-09 12:13:26 +0200

    added node snapshot domain object

---
+++ src/node-snap-shot.ts
@@ -0,0 +1,35 @@
+import {Node} from "./node";
+
+export class NodeSnapShot {
+    public startDate: Date;
+    public endDate: Date;
+    public node: Node;
+
+    constructor(startDate: Date, endDate: Date, node: Node) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.node = node;
+    }
+
+    toJSON():Object {
+        return {
+            startDate: this.startDate,
+            endDate: this.endDate,
+            node: this.node
+        }
+    }
+
+    static fromJSON(nodeSnapShot:string|Object):NodeSnapShot {
+        let snapShotObject: any;
+        if (typeof nodeSnapShot === 'string') {
+            snapShotObject = JSON.parse(nodeSnapShot);
+        } else
+            snapShotObject = nodeSnapShot;
+
+        return new NodeSnapShot(
+            snapShotObject.startDate,
+            snapShotObject.endDate,
+            Node.fromJSON(snapShotObject.node)
+        );
+    }
+}
