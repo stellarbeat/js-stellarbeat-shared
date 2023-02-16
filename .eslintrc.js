ef9c935c31c26e73900428283e7cf0614e9b6404
pieterjan84 <pieterjan84@users.noreply.github.com>
2023-02-16 14:47:01 +0100

    generating dto validation code on build

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
 	},
 	env: {
 		node: true
-	}
+	},
+	ignorePatterns: ['/*', '!/src']
 };
---
+++ generate-dto-validate-functions.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+const path = require("path")
+const Ajv = require("ajv")
+const addFormats = require("ajv-formats");
+
+const {
+    NodeV1Schema,
+    NodeSnapshotV1Schema,
+    OrganizationV1Schema,
+    OrganizationSnapshotV1Schema,
+    NetworkV1Schema
+} = require("./lib");
+const standaloneCode = require("ajv/dist/standalone").default
+
+// For CJS, it generates an exports array, will generate
+// `exports["#/definitions/Foo"] = ...;exports["#/definitions/Bar"] = ... ;`
+const ajv = new Ajv({
+    schemas: [NodeV1Schema, NodeSnapshotV1Schema, OrganizationV1Schema, OrganizationSnapshotV1Schema, NetworkV1Schema],
+    code: {source: true }
+})
+addFormats(ajv);
+let moduleCode = standaloneCode(ajv)
+
+// Now you can write the module code to file
+fs.writeFileSync(path.join(__dirname, "./lib/dto/validate.js"), moduleCode)
